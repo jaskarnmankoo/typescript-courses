613e6a58cc7fb7847626f53d158e138ce015f2c7
Jaskarn Mankoo <jaskarn.mankoo@mail.utoronto.ca>
2024-07-06 17:45:51 -0400

    feat: done Generics Scopes and Constraints lesson

--- packages/notes-intermediate-ts-v2/src/06-generic-scopes-and-constraints.ts
+++ packages/notes-intermediate-ts-v2/src/06-generic-scopes-and-constraints.ts
@@ -20,24 +20,24 @@ const phoneDict = {
   /*... and so on */
 }
 
-function listToDict<T>(
-  list: T[], // array as input
-  idGen: (arg: T) => string, // fn for obtaining item's id
-): { [k: string]: T } {
-  // create dict to fill
-  const dict: { [k: string]: T } = {}
+// function listToDict<T>(
+//   list: T[], // array as input
+//   idGen: (arg: T) => string, // fn for obtaining item's id
+// ): { [k: string]: T } {
+//   // create dict to fill
+//   const dict: { [k: string]: T } = {}
 
-  for (let item of list) {
-    // for each item
-    dict[idGen(item)] = item // make a key store in dict
-  }
+//   for (let item of list) {
+//     // for each item
+//     dict[idGen(item)] = item // make a key store in dict
+//   }
 
-  return dict // result
-}
-/*
-// interface HasId {
-//   id: string
+//   return dict // result
 // }
+
+interface HasId {
+  id: string
+}
 // interface Dict<T> {
 //   [k: string]: T
 // }
@@ -52,76 +52,75 @@ function listToDict<T>(
 //   return dict
 // }
 
-/*
 //? Let's make it
 // function listToDict<T>(list: T[]): Dict<T> {
 
 //* Describing the constraint
-/*
-// function listToDict<T extends HasId>(list: T[]): Dict<T> {
+
+function listToDict<T extends HasId>(list: T[]): Dict<T> {}
 
 //* Scopes and Type Parameters
-/*
-// function eatApple(bowl: any, eater: (arg: any) => void) {}
 
-// function receiveFruitBasket(bowl: any) {
-//   console.log('Thanks for the fruit basket!')
-//   // only `bowl` can be accessed here
-//   eatApple(bowl, (apple: any) => {
-//     // both `bowl` and `apple` can be accessed here
-//   })
-// }
+function eatApple(bowl: any, eater: (arg: any) => void) {}
 
-// // outer function
-// function tupleCreator<T>(first: T) {
-//   // inner function
-//   return function finish<S>(last: S): [T, S] {
-//     return [first, last]
-//   }
-// }
-// const finishTuple = tupleCreator(3 as const)
-// const t1 = finishTuple(null)
-// const t2 = finishTuple([4, 8, 15, 16, 23, 42])
+function receiveFruitBasket(bowl: any) {
+  console.log('Thanks for the fruit basket!')
+  // only `bowl` can be accessed here
+  eatApple(bowl, (apple: any) => {
+    // both `bowl` and `apple` can be accessed here
+  })
+}
 
-//* Best practices
-// interface HasId {
-//   id: string
-// }
-// interface Dict<T> {
-//   [k: string]: T
-// }
+// outer function
+function tupleCreator<T>(first: T) {
+  // inner function
+  return function finish<S>(last: S): [T, S] {
+    return [first, last]
+  }
+}
+const finishTuple = tupleCreator(3 as const)
+const t1 = finishTuple(null)
+const t2 = finishTuple([4, 8, 15, 16, 23, 42])
 
-// function example1<T extends HasId[]>(list: T) {
-//   return list.pop()
-//   //      ^?
-// }
-// function example2<T extends HasId>(list: T[]) {
-//   return list.pop()
-//   //      ^?
-// }
+// Best practices
+interface HasId {
+  id: string
+}
+interface Dict<T> {
+  [k: string]: T
+}
 
-// class Payment implements HasId {
-//   static #next_id_counter = 1;
-//   id = `pmnt_${Payment.#next_id_counter++}`
-// }
-// class Invoice implements HasId {
-//   static #next_id_counter = 1;
-//   id = `invc_${Invoice.#next_id_counter++}`
-// }
+function example1<T extends HasId[]>(list: T) {
+  return list.pop()
+  //      ^?
+}
+function example2<T extends HasId>(list: T[]) {
+  return list.pop()
+  //      ^?
+}
 
-// const result1 = example1([
-//   //   ^?
-//   new Payment(),
-//   new Invoice(),
-//   new Payment()
-// ])
+class Payment implements HasId {
+  static #next_id_counter = 1
+  id = `pmnt_${Payment.#next_id_counter++}`
+}
+class Invoice implements HasId {
+  static #next_id_counter = 1
+  id = `invc_${Invoice.#next_id_counter++}`
+}
 
-// const result2 = example2([
-//   //   ^?
-//   new Payment(),
-//   new Invoice(),
-//   new Payment()
-// ])
+const result1 = example1([
+  //   ^?
+  new Payment(),
+  new Invoice(),
+  new Payment(),
+])
+
+const result2 = example2([
+  //   ^?
+  new Payment(),
+  new Invoice(),
+  new Payment(),
+])
 
 /**/
 export default {}
